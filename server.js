bae252b17ba8789baf77f44c0742d6d887b12571
Felipe Mult <felipeteixeira@mult-e.com.br>
2018-05-15 23:34:14 -0300

    heroku deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var history = require('connect-history-api-fallback');
+
+app.use(history());
+
+app.use(express.static(__dirname + '/app/'));
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'));
